3874084a1e72c7e7a8fb0defcf7763a26837db66
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2021-10-13 21:00:55 -0500

    taller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BookModule } from './book/book.module';
+import { EditorialModule } from './editorial/editorial.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BookModule,
+    EditorialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
